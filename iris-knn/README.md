400876ed9073441bafba4a7fcd13cf9b0bfc0761
Eshan Mewantha <eshanm@hsenidmobile.com>
2017-07-12 22:48:55 +0530

    Adding helper files

--- iris-knn/README.md
+++
@@ -1,8 +0,0 @@
-since the target has 3 classes, k is better to be set with something other than a multiple of 3
-
-
-iris_nearest_neighbour_pure_python 
-
-k = 1   accuracy = 93.3333333333%
-k = 4   accuracy = 96.6666666667%
-k = 9   accuracy = 100.0%
---
+++ iris-knn/iris_sckit_learn.py
@@ -0,0 +1,51 @@
+__author__ = 'eshan'
+
+from math import sqrt
+import operator
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def euclidean_distance(example1, example2):
+    return sqrt(sum((e1 - e2) ** 2 for e1, e2 in zip(example1, example2)))
+
+
+def get_prediction(test_instance):
+    global training
+    global k
+    distances = []
+
+    for _, r in training.iterrows():
+        distance = euclidean_distance(r[:-1].tolist(), test_instance.tolist())
+        neighbour = r[-1]
+        distances.append((distance, neighbour))
+
+    distances.sort(key=operator.itemgetter(0))
+
+    candidates = [v for _, v in distances[:k]]
+    return max(candidates, key=candidates.count)
+
+
+training = pd.read_csv('iris_training.csv')
+test = pd.read_csv('iris_test.csv')
+correct_predictions = 0
+k = 5                                             # number of neighbours to be considered in decision making
+
+
+
+knn = KNeighborsClassifier(n_neighbors=k)
+knn.fit(training[:-1], training[-1])
+predictions = knn.predict(test)
+print(predictions)
+
+# for index, row in test.iterrows():
+#     prediction = get_prediction(row[:-1])
+#     if prediction == row[-1]:
+#         correct_predictions += 1
+#
+#
+# print('Correct predictions : ' + str(correct_predictions))
+# print('Total   predictions : ' + str(test.shape[0]))
+# print('Accuracy ' + str(correct_predictions*100.0/test.shape[0]) + '%')
+
+
